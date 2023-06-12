219922d006da3c8f66da72700eea9853e5740144
nataliaventura93 <nataliaventura93@gmail.com>
2023-06-12 09:58:57 +0200

    Add functions and fetch to give html cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,36 @@
 'use strict';
 
-const card = document.querySelector('.js_card');
+let ulList = document.querySelector('.js_card_list');
 const input = document.querySelector('.js_input');
 const btnSearch = document.querySelector('.js_btnSearch');
 
-const URL = 'api.disneyapi.dev/character?pageSize=50';
+const URL = 'https://dev.adalab.es/api/disney?pageSize=50';
 let cardList = [];
+
+function renderCard(cardData) {
+  const card = `<li class="card">
+      <article>
+        <img
+          class="card_img"
+          src="${cardData.imageUrl}"
+          alt="disney_character"
+        />
+        <p class="card_name">${cardData.name}</p>
+      </article>
+    </li>`;
+  return card;
+}
+
+function renderCardList(cardDataList) {
+  ulList.innerHTML = '';
+  for (const cardItem of cardDataList) {
+    ulList.innerHTML += renderCard(cardItem);
+  }
+}
 
 fetch(URL)
   .then((response) => response.json())
   .then((data) => {
-    
+    cardList = data.data;
+    renderCardList(cardList);
   });
